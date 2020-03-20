36000cc7ec70683d161eeccec537ff3ae5d81eb4
MIchaelJier <chem908430737@icloud.com>
2020-03-20 23:41:22 +0800

    修改 接口地址

--- app/router/api.js
+++ app/router/api.js
@@ -1,3 +1,4 @@
-exports.baseUrl = 'https://zfroot.top:8081/yun'
+//exports.baseUrl = 'https://zfroot.top:8081/yun'
+exports.baseUrl = 'https://hzt.rioweb.cn/yun'
 
 exports.mockUrl = 'http://www.yingjiechen.cn:9000/mock/11'
